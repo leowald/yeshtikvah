6c75b0ae24a3da6b67459a3eb5da763da5dac635
zippy s <zippy1043@gmail.com>
2024-11-28 14:09:21 +0200

    changes to Error component test

--- front/tests/components/Error.test.jsx
+++ front/tests/components/Error.test.jsx
@@ -3,26 +3,17 @@ import { render, screen } from "@testing-library/react";
 import Error from "../../src/components/Error";
 
 describe("tests for Error component", () => {
-  function renderComponent(children) {
-    render(<Error>{children}</Error>);
+  function renderComponent(errTxt) {
+    render(<Error errText={errTxt} />);
   }
 
-  it("should contain the text passed in as props.children", () => {
-    const childrenText = "error error error";
-    renderComponent(childrenText);
-    expect(screen.getByText(/error error error/i)).toBeInTheDocument();
+  it("should contain the text passed in", () => {
+    renderComponent("this is the message");
+    expect(screen.getByText(/this is the message/i)).toBeInTheDocument();
   });
 
-  it("should contain multiple elements passed through as props.children", () => {
-    const childrenText = (
-      <div>
-        <h1>Hello</h1>
-        <p>How are you</p>
-      </div>
-    );
-    renderComponent(childrenText);
-    screen.debug();
-    expect(screen.getByRole("heading", { name: /hello/i })).toBeInTheDocument();
-    expect(screen.getByRole("paragraph")).toHaveTextContent(/How are you/i);
+  it("should contain the words Error!", () => {
+    renderComponent("this is the message");
+    expect(screen.getByText(/error!/i)).toBeInTheDocument();
   });
 });
