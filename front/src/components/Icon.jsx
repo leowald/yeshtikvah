f77b3eb4ed726c214fa858baf6e8e3297945f6e4
Chani Lobenstein <hannahfriedman1426@gmail.com>
2024-11-28 22:42:18 +0200

    initial commit for 23-icon-component

---
+++ front/src/components/Icon.jsx
@@ -0,0 +1,48 @@
+import PropTypes from "prop-types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+library.add(fas, far, fab);
+
+/** This component takes in a top Icon (object) and an optional bottom Icon (object) and a size and creates a font-awesome incon accordingly */
+export default function Icon({ topIcon, size, bottomIcon, ...extras }) {
+  return (
+    <>
+      <div className={bottomIcon ? "fa-layers" : ""}>
+        {bottomIcon && (
+          <FontAwesomeIcon
+            data-testid="bottomIcon"
+            style={bottomIcon.style}
+            color={bottomIcon.color}
+            icon={`${bottomIcon.name ? bottomIcon.name : "fa-circle"} ${
+              bottomIcon.type ? bottomIcon.type : "fas"
+            }`}
+            size={size}
+            fixedWidth
+          />
+        )}
+        <FontAwesomeIcon
+          data-testid="topIcon"
+          style={topIcon.style}
+          color={topIcon.color}
+          icon={`${topIcon.name} ${topIcon.type}`}
+          size={size}
+          {...extras}
+          fixedWidth
+          transform="shrink-6"
+        />
+      </div>
+    </>
+  );
+}
+
+Icon.propTypes = {
+  /** What icon to use as the top icon. This parameter is an object with the following properties: color,name,type, style(optional) and extras(optional). */
+  topIcon: PropTypes.object,
+  /** What icon to use as the bottom icon. This is an optional parameter.  */
+  bottomIcon: PropTypes.object,
+  /** What size the fa icon should be e.g. "3x" */
+  size: PropTypes.string,
+};
---
+++ front/src/components/Icon.stories.jsx
@@ -0,0 +1,27 @@
+import Icon from "./Icon.jsx";
+
+const topIcon = {
+  color: "green",
+  name: "fa-house",
+  type: "fas",
+  style: { border: "1px solid #FF7888", borderRadius: "50%" },
+};
+
+const bottomIcon = {
+  color: "pink",
+  style: { border: "1px solid #FF7888", borderRadius: "50%" },
+};
+
+export default {
+  component: Icon,
+};
+
+export const FALayeredIconComponent = {
+  args: { topIcon: topIcon, bottomIcon: bottomIcon, size: "3x" },
+  tags: ["autodocs"],
+};
+
+export const FAIconComponent = {
+  args: { topIcon: topIcon, size: "3x" },
+  tags: ["autodocs"],
+};
