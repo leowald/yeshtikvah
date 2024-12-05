63bd790045585a3971c37fba68bf45186e138fd8
Shifra Kupferstein <shk56893@gmail.com>
2024-12-05 11:50:36 +0200

    documentation for categoryContext

--- front/src/store/categoryContext.jsx
+++ front/src/store/categoryContext.jsx
@@ -1,4 +1,10 @@
 import { createContext } from "react";
 
+/**
+ * In order to avoid prop drilling we will use context.
+ * This componenet creates a new context in React using createContext() and exports it as categoryContext. This context can be used to share data across components in a React application following the useContext hook.
+ * @returns {object} categoryContext
+ */
+
 const categoryContext = createContext();
 export default categoryContext;
