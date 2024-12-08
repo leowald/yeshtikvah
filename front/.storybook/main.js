67b82292def90d0a2987e16c3365d3b89d54c4cb
Shifra Kupferstein <shk56893@gmail.com>
2024-12-08 11:29:13 +0200

    removed mdx file

--- front/.storybook/main.js
+++ front/.storybook/main.js
@@ -10,10 +10,9 @@ const config = {
     {
       name: "storybook-addon-jsdoc-to-mdx",
       options: {
-        folderPaths: ["./src/hooks", "./src/utils"], // paths to folders with JS/TS code
+        folderPaths: ["./src/hooks", "./src/utils", "./src/store"], // paths to folders with JS/TS code
 
         extensions: ["ts", "js", "jsx", "mjs"], // file extensions to include
-
       },
     },
   ],
