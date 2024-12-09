bec7dfb6633d373fbab24789dafc049f42d94808
Shifra Kupferstein <shk56893@gmail.com>
2024-12-09 12:04:29 +0200

    commit

--- front/.storybook/preview.js
+++ front/.storybook/preview.js
@@ -1,6 +1,8 @@
 /** @type { import('@storybook/react').Preview } */
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import "!style-loader!css-loader!sass-loader!../src/styles/style.scss";
+import "!style-loader!css-loader!../node_modules/@fortawesome/fontawesome-free/css/all.css";
 
 const preview = {
   parameters: {
