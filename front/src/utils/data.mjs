2a40a2878a2825b8eb654a825360715ad1e2a9d6
Shifra Kupferstein <shk56893@gmail.com>
2024-12-11 11:26:53 +0200

    looks good

--- front/src/utils/data.mjs
+++ front/src/utils/data.mjs
@@ -8,60 +8,50 @@ export const data = {
   stories: [],
 };
 
-function ConvertToSlug(title) {
-  title = title.toLowerCase(); //t is the title received
-  title = title.trim(); // trim the spaces from start and end
-  var slug = title.replace(/[^a-z0-9]+/g, "-"); // replace all the spaces with "-"
-  return slug;
-}
-
 const categoryID = [];
-const childrenID = [];
 
 export function createRandomCategory() {
   const name = faker.word.noun({ length: { min: 3, max: 8 } });
   const id = faker.database.mongodbObjectId();
   categoryID.push(id);
-  const categoryItem = {
+
+  const category_children = [];
+
+  //creating category children
+  for (let index = 0; index < faker.number.int(3); index++) {
+    const subCategoryID = faker.database.mongodbObjectId();
+    categoryID.push(subCategoryID);
+    const subCategoryName = faker.word.noun({ length: { min: 3, max: 8 } });
+
+    category_children.push({
+      id: subCategoryID,
+      count: faker.number.int(1000),
+      description: faker.lorem.sentence(),
+      slug: faker.helpers.slugify(subCategoryName),
+      name: subCategoryName,
+      image: faker.image.avatar(),
+      iconColor: [faker.color.rgb(), faker.color.rgb()],
+      color: faker.color.rgb(),
+    });
+  }
+  function total(total, num) {
+    return (total += num);
+  }
+
+  return {
     id: id,
-    count: faker.number.int(1000),
+    count:
+      category_children.length > 0
+        ? category_children.map((item) => item.count).reduce(total)
+        : faker.number.int(1000),
     description: faker.lorem.sentence(),
     slug: faker.helpers.slugify(name),
-    //category_children: category_children.length > 0 ? category_children : 0,
     name: name,
     image: faker.image.avatar(),
     iconColor: [faker.color.rgb(), faker.color.rgb()],
     color: faker.color.rgb(),
+    category_children: category_children.length > 0 ? category_children : 0,
   };
-
-  let isSubCategory = faker.datatype.boolean();
-  const category_children = [];
-  isSubCategory && childrenID.push(categoryItem);
-
-  if (!isSubCategory) {
-    for (let index = 0; index < faker.number.int(3); index++) {
-      if (childrenID.length > 0) {
-        let rnd = faker.number.int(childrenID.length - 1);
-        category_children.push(childrenID[rnd]);
-        childrenID.splice(rnd, 1);
-      }
-      
-    }
-
-    //const parentID = categoryID[faker.number.int(categoryID.length - 1)];
-
-    return {
-      id: id,
-      count: categoryItem.count,
-      description: categoryItem.description,
-      slug: categoryItem.slug,
-      category_children: category_children.length > 0 ? category_children : 0,
-      name: name,
-      image: categoryItem.image,
-      iconColor: categoryItem.iconColor,
-      color: categoryItem.color,
-    };
-  }
 }
 
 Array.from({ length: 8 }).forEach(() => {
@@ -72,7 +62,7 @@ export function createRandomStory() {
   return {
     id: faker.database.mongodbObjectId(),
     date: faker.date.recent().toLocaleDateString(),
-    catagaory_id: data.categories[faker.number.int(2)].id,
+    catagaory_id: categoryID[faker.number.int(categoryID.length - 1)],
     source: faker.lorem.word(),
     views: faker.number.int(500),
     shares: faker.number.int(500),
