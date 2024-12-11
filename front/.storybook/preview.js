0bc14ef8064d9bf871bf3f8ef4c8cb2920005159
Shifra Kupferstein <shk56893@gmail.com>
2024-12-11 10:11:15 +0200

    changes done

--- front/.storybook/preview.js
+++ front/.storybook/preview.js
@@ -1,9 +1,5 @@
 /** @type { import('@storybook/react').Preview } */
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import "!style-loader!css-loader!sass-loader!../src/styles/style.scss";
-import "!style-loader!css-loader!../node_modules/@fortawesome/fontawesome-free/css/all.css";
-
 const preview = {
   parameters: {
     controls: {
