f96fe032a74a27e0dbcf874a44f1ebed1e1a1ca9
Chani Lobenstein <hannahfriedman1426@gmail.com>
2025-01-15 11:59:15 +0200

    commiting first 2 changes without changing the url just because I don't want to lose them

--- front/src/components/nav/Logo.jsx
+++ front/src/components/nav/Logo.jsx
@@ -1,10 +1,11 @@
 import PropTypes from "prop-types";
-import "./Logo.scss";
+import styles from "./Logo.module.scss";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Error from "../Error";
 import axiosClient from "../../api/axiosClient.js";
 import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
 
 /**
  * This component displays the logo of the website.
@@ -33,9 +34,9 @@ export default function Logo({ size }) {
     <>
       <Container>
         <Link to="/">
-          <img
+          <Image
             role="logo"
-            className={`logo ${size}`}
+            className={`${styles.logo} ${styles[size]}`}
             src={
               size === "xl" ? logo[0]?.image.lg : `${logo[0]?.image[`${size}`]}`
             }
